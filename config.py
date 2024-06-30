7e06015d44da042afcb4a7a3432183907b5b1098
𖢵 || 𝗦ʏɴαxx ||𖢵 <159287148+Sagexdd@users.noreply.github.com>
2024-06-30 23:23:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 API_ID = "21827814"
-API_HASH = "af097d21fa5049b0ed0d614ed3b885cd
+API_HASH = "af097d21fa5049b0ed0d614ed3b885cd"
 BOT_TOKEN = "7369149161:AAFAoyZ818kS-St-mpcy2TTumzFlLFZWAX8"
 OWNER_ID = "7376728791"
 MONGO_URL = "mongodb+srv://bikash:bikash@bikash.3jkvhp7.mongodb.net/?retryWrites=true&w=majority"
